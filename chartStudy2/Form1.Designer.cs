1e9419946e9ab1e0d6f0b01d78c9c3d0b9d85755
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-02-18 12:03:52 +0900

    초기 커밋.

---
+++ chartStudy2/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace chartStudy2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.btnData = new System.Windows.Forms.Button();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart7 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart9 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart9)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnData
+			// 
+			this.btnData.Location = new System.Drawing.Point(12, 12);
+			this.btnData.Name = "btnData";
+			this.btnData.Size = new System.Drawing.Size(75, 23);
+			this.btnData.TabIndex = 0;
+			this.btnData.Text = "btnData";
+			this.btnData.UseVisualStyleBackColor = true;
+			this.btnData.Click += new System.EventHandler(this.btnData_Click);
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(12, 41);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(300, 300);
+			this.chart1.TabIndex = 1;
+			this.chart1.Text = "chart1";
+			// 
+			// chart2
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea2);
+			legend2.Name = "Legend1";
+			this.chart2.Legends.Add(legend2);
+			this.chart2.Location = new System.Drawing.Point(318, 41);
+			this.chart2.Name = "chart2";
+			series2.ChartArea = "ChartArea1";
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.chart2.Series.Add(series2);
+			this.chart2.Size = new System.Drawing.Size(300, 300);
+			this.chart2.TabIndex = 2;
+			this.chart2.Text = "chart2";
+			// 
+			// chart3
+			// 
+			chartArea3.Name = "ChartArea1";
+			this.chart3.ChartAreas.Add(chartArea3);
+			legend3.Name = "Legend1";
+			this.chart3.Legends.Add(legend3);
+			this.chart3.Location = new System.Drawing.Point(624, 41);
+			this.chart3.Name = "chart3";
+			series3.ChartArea = "ChartArea1";
+			series3.Legend = "Legend1";
+			series3.Name = "Series1";
+			this.chart3.Series.Add(series3);
+			this.chart3.Size = new System.Drawing.Size(300, 300);
+			this.chart3.TabIndex = 3;
+			this.chart3.Text = "chart3";
+			// 
+			// chart6
+			// 
+			chartArea4.Name = "ChartArea1";
+			this.chart6.ChartAreas.Add(chartArea4);
+			legend4.Name = "Legend1";
+			this.chart6.Legends.Add(legend4);
+			this.chart6.Location = new System.Drawing.Point(12, 357);
+			this.chart6.Name = "chart6";
+			series4.ChartArea = "ChartArea1";
+			series4.Legend = "Legend1";
+			series4.Name = "Series1";
+			this.chart6.Series.Add(series4);
+			this.chart6.Size = new System.Drawing.Size(300, 300);
+			this.chart6.TabIndex = 6;
+			this.chart6.Text = "chart6";
+			// 
+			// chart7
+			// 
+			chartArea5.Name = "ChartArea1";
+			this.chart7.ChartAreas.Add(chartArea5);
+			legend5.Name = "Legend1";
+			this.chart7.Legends.Add(legend5);
+			this.chart7.Location = new System.Drawing.Point(318, 357);
+			this.chart7.Name = "chart7";
+			series5.ChartArea = "ChartArea1";
+			series5.Legend = "Legend1";
+			series5.Name = "Series1";
+			this.chart7.Series.Add(series5);
+			this.chart7.Size = new System.Drawing.Size(300, 300);
+			this.chart7.TabIndex = 7;
+			this.chart7.Text = "chart7";
+			// 
+			// chart9
+			// 
+			chartArea6.Name = "ChartArea1";
+			this.chart9.ChartAreas.Add(chartArea6);
+			legend6.Name = "Legend1";
+			this.chart9.Legends.Add(legend6);
+			this.chart9.Location = new System.Drawing.Point(624, 357);
+			this.chart9.Name = "chart9";
+			series6.ChartArea = "ChartArea1";
+			series6.Legend = "Legend1";
+			series6.Name = "Series1";
+			this.chart9.Series.Add(series6);
+			this.chart9.Size = new System.Drawing.Size(300, 300);
+			this.chart9.TabIndex = 9;
+			this.chart9.Text = "chart9";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(959, 677);
+			this.Controls.Add(this.chart9);
+			this.Controls.Add(this.chart7);
+			this.Controls.Add(this.chart6);
+			this.Controls.Add(this.chart3);
+			this.Controls.Add(this.chart2);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.btnData);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart9)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnData;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart6;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart7;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart9;
+	}
+}
+
---
+++ chartStudy2/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace chartStudy2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+    private void btnData_Click(object sender, EventArgs e)
+    {
+    
+  
+      // (1) Y값 배열 데이타바인딩
+      double[] scores = new double[] { 80, 90, 85, 70, 95 };
+      chart1.Series[0].Points.DataBindY(scores);
+
+      // (2) X, Y값 List<T> 데이타바인딩
+      List<string> x = new List<string> { "철수", "영희", "길동", "재동", "민희" };
+      List<double> y = new List<double> { 80, 90, 85, 70, 95 };
+      chart2.Series[0].Points.DataBindXY(x, y);
+
+      // (3) 객체 컬렉션에 대한 데이타바인딩
+      List<Student> students = new List<Student>();
+      students.Add(new Student("철수", 80));
+      students.Add(new Student("영희", 90));
+      students.Add(new Student("길동", 85));
+      // X축: Name, Y축: Score
+      chart3.Series[0].Points.DataBind(students, "Name", "Score", null);
+
+      string strConn = "datasource=127.0.0.1;port=3306;database=dawoon;username=root;password=ekdnsel;Charset=utf8";
+      string sql = "SELECT 문서.careSeq, 등록.number, 문서.careStart, 문서.careFinish, 문서.time, 문서.sympton, 문서.count, 문서.injection, " +
+        "문서.oral, 등록.enrollSeq, 문서.age, 등록.birth, 문서.memo, 문서.flagYN, 문서.regDate, 문서.issueDate, 문서.issueID FROM dc_careenroll " +
+        "등록 inner JOIN dc_caredocument 문서 ON(등록.enrollSeq = 문서.enrollSeq) WHERE 문서.flagYN = 'Y' GROUP BY 문서.number";
+
+
+      // (2) DataSource와 DataBind() 사용
+      using (MySqlConnection conn = new MySqlConnection(strConn))
+      {
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+        DataSet ds = new DataSet();
+        MySqlDataAdapter sa = new MySqlDataAdapter(cmd);
+        sa.Fill(ds);
+
+        // DataTable 객체를 DataSource에 지정하고,
+        // X,Y축 컬럼을 XValueMember와 YValueMembers에 지정
+        chart6.DataSource = ds.Tables[0];
+        chart6.Series[0].XValueMember = "number";
+        chart6.Series[0].YValueMembers = "time";
+        chart6.DataBind();
+      }
+
+      // (3) Points.DataBind() 사용
+      using (MySqlConnection conn = new MySqlConnection(strConn))
+      {
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+        MySqlDataReader dataReader = cmd.ExecuteReader();
+
+        // IDataReader 객체와 X, Y축 컬럼 지정. 
+        // 4번째 Param: 툴팁과 같은 다른 필드 지정가능. 여기선 sales 컬럼값을 툴팁에 표시
+        chart7.Series[0].Points.DataBind(dataReader, "number", "time", "Tooltip=time");
+
+        // DataBindCrossTable() 사용
+
+        DataTable dt = new DataTable("Order");
+        dt.Columns.Add("customer");
+        dt.Columns.Add("product");
+        dt.Columns.Add("orders");
+        dt.Rows.Add("Tom", "USB", 10);
+        dt.Rows.Add("Tom", "HDD", 2);
+        dt.Rows.Add("Tom", "Monitor", 1);
+        dt.Rows.Add("Jane", "USB", 3);
+        dt.Rows.Add("Jane", "HDD", 1);
+        dt.Rows.Add("Jane", "Monitor", 2);
+
+        // Product별로 Series를 하나씩 자동으로 생성
+        // X축은 customer 컬럼, Y축은 orders 컬럼
+        // 각 그래프 상단에 product명으로 Label을 붙임
+        chart9.DataBindCrossTable(dt.AsEnumerable(), "product", "customer", "orders", "Label=product");
+      }
+
+
+    }
+    class Student
+    {
+      public string Name { get; set; }
+      public double Score { get; set; }
+
+      public Student(string name, double score)
+      {
+        this.Name = name;
+        this.Score = score;
+
+
+      }
+    }
+    private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
